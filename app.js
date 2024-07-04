882c70b6bd24bf34b6a9bb876dd2e97ff675fa5e
Arthur (armando808) <arthur.grimley@hotmail.com>
2024-07-04 15:32:23 +0100

    delete error handling and custom error handling for user_id and route_id

--- app.js
+++ app.js
@@ -41,31 +41,30 @@ app.delete('/api/users/:user_id/user_routes/:route_id', deleteUserRoute)
 //404 if the user id or route id does not exist
 //400 if user_id or route id is invalid
 
-//errors in the endpoints
 app.all("*", (req, res) => {
-    res.status(404).send({ msg: "Not Found" });
-  });
+  res.status(404).send({ msg: "Not Found" });
+});
 
-  app.use((err, req, res, next) => {
-    if (err.code) {
-      res.status(400).send({ msg: "Bad Request" });
-    } else {
-      next(err);
-    }
-  });
-  
-  app.use((err, req, res, next) => {
-    if (err.msg) {
-      res.status(err.status).send({ msg: err.msg });
-    } else {
-      next(err);
-    }
-  });
-  
-  app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).send({ msg: "Internal Server Error" });
-  });
+app.use((err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  if (err.code === '22P02') {
+    res.status(400).send({ msg: "Bad Request" });
+  } else {
+    next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send({ msg: "Internal Server Error" });
+});
 
 
 module.exports = app
