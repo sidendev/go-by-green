b2d4feca4f125c8586df90284b23646669372b8d
Mark Harris <markymark1990@hotmail.co.ukgit config --global user.name markymark1990lsgit config --global user.email markymark1990@hotmail.co.uk>
2024-07-05 16:48:30 +0100

    map in progress

--- front_end/go_by_green/go_by_green_app/components/MapComponent.tsx
+++ front_end/go_by_green/go_by_green_app/components/MapComponent.tsx
@@ -1,12 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { View, StyleSheet, Dimensions, Button } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
-import axios from 'axios';
-import { GOOGLE_MAPS_API_KEY } from '@env';
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  StyleSheet,
+  Dimensions,
+  Button,
+  Pressable,
+  Text,
+} from "react-native";
+import MapView, { Marker, Region } from "react-native-maps";
+import axios from "axios";
+import { GOOGLE_MAPS_API_KEY } from "@env";
 
-console.log(GOOGLE_MAPS_API_KEY)
-
-const address = '24%20Sussex%20Drive%20Ottawa%20ON';
+const address = "10 Downing St London SW1A 2AA";
 
 const MapComponent: React.FC = () => {
   const [location, setLocation] = useState<{
@@ -14,23 +19,37 @@ const MapComponent: React.FC = () => {
     longitude: number;
   } | null>(null);
 
+  const [region, setRegion] = useState<Region>({
+    latitude: 53.480759,
+    longitude: -2.242631,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
   useEffect(() => {
     const fetchCoordinates = async () => {
       try {
         const response = await axios.get(
-          'https://maps.googleapis.com/maps/api/geocode/json',
+          "https://maps.googleapis.com/maps/api/geocode/json",
           {
             params: {
-              address: address,
+              address: "CWC8%2BR9%20Mountain%20View%20CA%20USA",
               key: GOOGLE_MAPS_API_KEY,
             },
           }
         );
         const location = response.data.results[0].geometry.location;
-        console.log(fetchCoordinates, location)
+        console.log(response)
+        console.log(">>>>>>", fetchCoordinates, location);
         setLocation({
           latitude: location.lat,
           longitude: location.lng,
+        });
+        setRegion({
+          latitude: location.lat,
+          longitude: location.lng,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
         });
       } catch (error) {
         console.error(error);
@@ -41,60 +60,78 @@ const MapComponent: React.FC = () => {
   }, []);
 
   const onPressLearnMore = () => {
-    return console.log("Hello")
-  }
+    if (location) {
+      console.log("Hello?");
+      setRegion({
+        ...region,
+        latitude: location.latitude,
+        longitude: location.longitude,
+      });
+    }
+  };
 
   return (
     <View style={styles.container}>
-
-        <View style={styles.map}>
-      <MapView
-      style={{flex: 1}}
-        initialRegion={{
-          latitude: 53.480759,
-          longitude: -2.242631,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      >
-        {location && (
-          <Marker
-            coordinate={location}
-            title="Location"
-            description={address}
-          />
-        )}
-      </MapView>
+      <View style={styles.map}>
+        <MapView
+          style={{ flex: 1 }}
+          initialRegion={{
+            latitude: 53.480759,
+            longitude: -2.242631,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          {location && (
+            <Marker
+              coordinate={location}
+              title="Location"
+              description={address}
+            />
+          )}
+        </MapView>
       </View>
-      
+
       <View style={styles.buttonView}>
-        <Button
-        style= {{height:"50%"}}
-  onPress={onPressLearnMore}
-  title="Learn More"
-  color="#841584"
-  accessibilityLabel="Learn more about this purple button"
-/>
+        <Pressable style={styles.button} onPress={onPressLearnMore}>
+          <Text style={styles.text}>Click me!</Text>
+        </Pressable>
+      </View>
     </View>
-
-    </View>
-
-
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     height: "100%",
-    width: "100%"
+    width: "100%",
   },
   map: {
-    height: "50%",
+    height: "94%",
   },
   buttonView: {
-    height: "50%",
-    width: "80%"
-  }
+    height: "30%",
+    width: "100%",
+  },
+  button: {
+    height: "20%",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: "green",
+  },
+  text: {
+    fontSize: 16,
+  },
 });
 
 export default MapComponent;
+
+
+
+
+
+
