81944a1f9257e47a6f9146c70abc6eaf291a7163
sidendev <96108993+sidendev@users.noreply.github.com>
2024-09-09 23:29:31 +0100

    Update api address for axios calls

--- application/ApiCalls/SpecificUserApi.jsx
+++ application/ApiCalls/SpecificUserApi.jsx
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const fetchSpecificUser = (userId) =>{
-    let baseURL = `https://gobygreen.onrender.com/api/users/${userId}/`
+const fetchSpecificUser = (userId) => {
+  let baseURL = `https://go-by-green.onrender.com/api/users/${userId}/`
 
-    return axios.get(baseURL).then((response)=>{return response.data})
-    .catch((err)=>{
-        console.log(err, 'error getting specific user.')
+  return axios.get(baseURL).then((response) => { return response.data })
+    .catch((err) => {
+      console.log(err, 'error getting specific user.')
     })
 }
 
-export {fetchSpecificUser}
+export { fetchSpecificUser }
--- application/ApiCalls/UserRoutesAPI.jsx
+++ application/ApiCalls/UserRoutesAPI.jsx
@@ -2,13 +2,12 @@ import axios from "axios";
 
 
 const getRoutesByUserId = (userId) => {
-    console.log(userId, '<<<<<<<<<<apilog')
-    let baseURL = `https://gobygreen.onrender.com/api/users/${userId}/user_routes`
+  let baseURL = `https://go-by-green.onrender.com/api/users/${userId}/user_routes`
 
-    return axios.get(baseURL).then((response) => { return response.data })
-        .catch((err) => {
-            console.log(err, 'error from getting user routes.')
-        })
+  return axios.get(baseURL).then((response) => { return response.data })
+    .catch((err) => {
+      console.log(err, 'error from getting user routes.')
+    })
 }
 
 
