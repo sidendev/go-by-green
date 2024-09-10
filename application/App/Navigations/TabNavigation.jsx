96de5a48e2b62e67dda7ec9bf737a6cf691e54e6
sidendev <96108993+sidendev@users.noreply.github.com>
2024-09-10 17:16:00 +0100

    Update layout to tab navigation and change colours

--- application/App/Navigations/TabNavigation.jsx
+++ application/App/Navigations/TabNavigation.jsx
@@ -1,27 +1,43 @@
 // navigation/TabNavigation.js
 import { StyleSheet, Text, View } from "react-native";
-import React, {useState, useContext} from "react";
-import LoginScreen from "../Screen/LoginScreen/LoginScreen";
+import React, { useState, useContext } from "react";
 import ProfileScreen from "../Screen/ProfileScreen/ProfileScreen";
 import SavedRoutes from "../Screen/SavedRoutes/SavedRoutes";
 import HomeScreen from "../Screen/HomeScreen/HomeScreen";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import Colours from "../Utils/Colours";
 
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigation() {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
+    <Tab.Navigator screenOptions={{
+      headerShown: false,
+      tabBarStyle: {
+        height: 70, // height of the tab bar
+        paddingTop: 10, // padding above the icon
+        paddingBottom: 10, // padding below the label
+      },
+      tabBarLabelStyle: {
+        fontSize: 12,
+        fontFamily: 'Poppins-SemiBold',
+        fontWeight: '600',
+      },
+      tabBarIconStyle: {
+        marginBottom: 5,
+      },
+    }}>
       <Tab.Screen
         name="HomeScreen"
         component={HomeScreen}
         options={{
           tabBarLabel: "Search",
-          tabBarActiveTintColor: Colours.PRIMARY,
+          tabBarActiveTintColor: Colours.GOBYGREEN_GREEN,
           tabBarIcon: ({ color, size }) => (
-            <Ionicons name="search" size={size} color={color} />
+            <MaterialCommunityIcons name="map-search-outline" size={size} color={color} />
           ),
         }}
       />
@@ -29,10 +45,10 @@ export default function TabNavigation() {
         name="SavedRoutes"
         component={SavedRoutes}
         options={{
-          tabBarLabel: "Saved Routes",
-          tabBarActiveTintColor: Colours.PRIMARY,
+          tabBarLabel: "My Routes",
+          tabBarActiveTintColor: Colours.GOBYGREEN_GREEN,
           tabBarIcon: ({ color, size }) => (
-            <Ionicons name="heart" size={size} color={color} />
+            <FontAwesome name="bookmark" size={size} color={color} />
           ),
         }}
       />
@@ -41,7 +57,7 @@ export default function TabNavigation() {
         component={ProfileScreen}
         options={{
           tabBarLabel: "Profile",
-          tabBarActiveTintColor: Colours.PRIMARY,
+          tabBarActiveTintColor: Colours.GOBYGREEN_GREEN,
           tabBarIcon: ({ color, size }) => (
             <Ionicons name="person" size={size} color={color} />
           ),
--- application/App/Utils/Colours.js
+++ application/App/Utils/Colours.js
@@ -4,5 +4,6 @@ export default {
   HIGHLIGHT_GREEN: '#36D67E',
   WHITE: 'white',
   BLACK: 'black',
+  GOBYGREEN_GREEN: '#00BF63',
   WHITE_TRANSPARANT: '#ffffff87',
 };
